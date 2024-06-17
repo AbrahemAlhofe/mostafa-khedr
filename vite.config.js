1a2bc48b0f6087e0094a2d5389e2e9e04046f285
Abrahem Alhofe <alhofeabrahem88@gmail.com>
2024-06-17 17:20:34 +0300

    fix: creative-file is not found

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import svgLoader from 'vite-svg-loader';
 import handlebars from 'vite-plugin-handlebars';
 
 export default {
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        ['creative-file']: resolve(__dirname, 'creative-file.html'),
+      },
+    },
+  },
   plugins: [
     svgLoader(),
     handlebars({
