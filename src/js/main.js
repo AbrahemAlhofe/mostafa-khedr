e1a32525a725ffacdc0e7c3dd827a444b39f723f
Abrahem Alhofe <alhofeabrahem88@gmail.com>
2024-06-17 17:25:25 +0300

    refactor: move main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-import "./styles/style.css";
+import "../styles/style.css";
 import SVGInjectInstance from "@iconfu/svg-inject";
 
 Array.from(document.querySelectorAll('[data-svg]')).forEach($wrapper => {
